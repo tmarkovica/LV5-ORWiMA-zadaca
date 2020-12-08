89c50d34cd6a35a7102ceb5058b516d8c4e4edf4
tmarkovica <73433147+tmarkovica@users.noreply.github.com>
2020-12-08 18:52:46 +0100

    Initial commit

---
+++ app/src/main/java/hr/ferit/tomislavmarkovica/lv5_orwima_zadaca/MainActivity.java
@@ -0,0 +1,14 @@
+package hr.ferit.tomislavmarkovica.lv5_orwima_zadaca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
